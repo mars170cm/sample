26bffab54e0cadb04d4ff03eea3ff73b40682218
langongwei <manageups@aliyun.com>
2018-05-25 05:26:14 +0000

    Add user following

--- routes/web.php
+++ routes/web.php
@@ -36,3 +36,10 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('passw
 
 Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);
 
+Route::get('/users/{user}/followings', 'UsersController@followings')->name('users.followings');
+Route::get('/users/{user}/followers', 'UsersController@followers')->name('users.followers');
+
+//关注用户
+Route::post('/users/followers/{user}', 'FollowersController@store')->name('followers.store');
+//取消关注
+Route::delete('/users/followers/{user}', 'FollowersController@destroy')->name('followers.destroy');
